b2d2a0d080c57a474349395f8cf2f810163802fc
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2022-11-22 01:55:59 +0400

    Update files

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,13 +1,13 @@
-import replace from 'gulp-replace'
-import plumber from 'gulp-plumber'
-import notify from 'gulp-notify'
+import replace     from 'gulp-replace'
+import plumber     from 'gulp-plumber'
+import notify      from 'gulp-notify'
 import browserSync from 'browser-sync'
-import newer from 'gulp-newer'
+import newer       from 'gulp-newer'
 
 export const plugins = {
-    replace: replace,
-    plumber: plumber,
-    notify: notify,
+    replace:     replace,
+    plumber:     plumber,
+    notify:      notify,
     browserSync: browserSync,
-    newer: newer,
+    newer:       newer,
 }
